72151257ed878fc2d14b39bc3a420c3740994ddb
Administrator <18372568672@139.com>
2020-01-12 10:26:09 +0800

    小明你修改

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("第一次修改"+1+"");
         System.out.println("第二次修改"+1+"");
         System.out.println("分支第一次修改"+1+2);
+        System.out.println("小明修改");
     }
 }
