401595373644ced0eb5e9eff188da16aa18436f0
Administrator <18372568672@139.com>
2020-01-12 10:12:21 +0800

    分支第一次修改

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("HelloWorld!");
         System.out.println("第一次修改"+1+"");
         System.out.println("第二次修改"+1+"");
+        System.out.println("分支第一次修改"+1+2);
     }
 }
